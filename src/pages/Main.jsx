67c4794d85137aac4d96d63d7368c2f2e4f2587c
Khushi Kalra <kalra.khushi12@gmail.com>
2024-06-04 17:01:15 +0530

    enhanced ui

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
-import { FaFilePdf, FaFileArchive } from 'react-icons/fa';
+import React, {useState} from "react";
+import { FaFilePdf, FaFileArchive, FaLockOpen} from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 import './Main.css';
+import icon from './icon.png';
 
 const Main = () => {
+  const navigate = useNavigate();
   const [zipFile, setZipFile] = useState(null);
   const [pdfFile, setPdfFile] = useState(null);
   const [result, setResult] = useState(null);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate('/');
+  };
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -41,10 +49,24 @@ const Main = () => {
 
   return (
     <div className="main-container">
+      <div className='main-header'>
+        <div className='icon-main-head'>
+        <img src={icon} alt="gb" className='main-icon' />
+        <h3>GradeBot.ai</h3>
+        </div>
+        <div className="user-info">
+            <button onClick={handleLogout} className="main-logout"><b>Logout</b> <FaLockOpen size={18} /></button>
+        </div>
+      </div>
+      <div className="main-text">
+        <h3>Hi, User.</h3>
+        <p>GradeBot is here to help you with grading answers. Let's make your task easy. Shall we?</p>
+      </div>
+      <div className='main-foot'>
       <div className="upload-icon">
         <label htmlFor="zip-upload">
           <FaFileArchive size={64} />
-          <p>Upload ZIP</p>
+          <p>Upload Student's Answers</p>
         </label>
         <input
           id="zip-upload"
@@ -57,7 +79,7 @@ const Main = () => {
       <div className="upload-icon">
         <label htmlFor="pdf-upload">
           <FaFilePdf size={64} />
-          <p>Upload PDF</p>
+          <p>Upload Answer Key</p>
         </label>
         <input
           id="pdf-upload"
@@ -67,12 +89,13 @@ const Main = () => {
           onChange={handleFileChange}
         />
       </div>
+      </div>
       <button onClick={handleFileUpload}>Upload Files</button>
 
       {result && (
         <div className="result">
           <p>Files processed successfully.</p>
-          <a href={`data:text/csv;charset=utf-8,${encodeURIComponent(result.csvData)}`} download="grading_results.csv">Download CSV</a>
+          <a href={`data:text/csv;charset=utf-8,${encodeURIComponent(result.csvData)}`} download="grading_results.csv"><span>Download CSV</span></a>
         </div>
       )}
     </div>
